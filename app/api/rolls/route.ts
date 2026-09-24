9d2780ba81844a43b7568e573578d30dacc12b2b
Regalus27 <16656788+Regalus27@users.noreply.github.com>
2026-09-24 17:47:06 -0500

    Updated rolls GET route to fix a merge error.

--- app/api/rolls/route.ts
+++ app/api/rolls/route.ts
@@ -73,18 +73,13 @@ export async function POST(request: NextRequest) {
   }
 }
 
-export async function GET(request: NextRequest) {
-    const uri = process.env.DB_URI!;
-    const client = new MongoClient(uri);
-
+export async function GET() {
     try {
-        const database = client.db("db");
+        const database = await getDatabase();
         const collection = database.collection("dice_rolls");
         const rolls = await collection.find().toArray();
         return NextResponse.json({ success: true, rolls }, { status: 200 });
     } catch (error) {
         return NextResponse.json({ success: false, error: "Server Error: Failed to fetch data." }, { status: 500 });
-    } finally {
-        await client.close();
     }
 }
